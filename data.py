621edb3772077c8106210c046cafdbe73cbe0603
Rishi Sharma <rsharma441@gmail.com>
2015-12-03 14:58:07 -0500

    added the Pi Gradient git status sss ..

--- data.py
+++ data.py
@@ -38,5 +38,71 @@ for i in range(len(sparse_slices)):
 	sparse_slices[i] = deleteElements(sparse_slices[i])
 
 
-print(l[0])
-print(sparse_slices[0])
+
+#Initialize Variables for optimization
+pattern_list = [] #list of coordinates
+alpha_list = []
+I = 10 #number of pattern matrices
+for i in range(I):
+    a = np.random.uniform(1,100, size = (7,7))
+    a_symm = (a+a.T)/2
+    pattern_list.append(a_symm)
+
+#linear weights
+alpha = np.random.uniform(0,.025,size = (I,N)) #do we need to change range of possible values?
+gamma = .5
+beta = .5
+lamb = 25
+omega = range(1,101)
+
+#list of coordinates to update
+
+for i in np.nditer(alpha):
+    alpha_list.append(i)
+
+#sgn function
+def sgn(n):
+    if n > 0:
+        return 1
+    elif n < 0:
+        return -1
+    elif n == 0:
+        return 0
+
+#projection function
+def proj(arr):
+    for element in np.nditer(arr, op_flags=['readwrite']):
+        if element < 1:
+            element[...] = 1
+        elif element > 100:
+            element[...] = 100
+
+    return(arr)
+
+#gradients
+def grad_pi(tensor_slices, pattern_list, alpha_list, pindex):
+	slist = []
+	
+	for n in range(len(tensor_slices)):
+		Tn = tensor_slices[n]
+		tensor_alphas = alpha_list[n::N]
+		print(len(tensor_alphas))
+		lincombo = pattern_list[0]*tensor_alphas[0]
+		for i in range(1,I):
+			lincombo+= tensor_alphas[i]*pattern_list[i]
+
+		diff = Tn - lincombo
+		alpha_i = tensor_alphas[pindex]
+		grad = diff*(-1)*alpha_i
+		slist.append(grad)
+
+	sumlist = sum(slist)
+
+	return(sumlist)
+
+
+test = grad_pi(sparse_slices,pattern_list,alpha_list,0)
+test1 = pattern_list[0] - gamma*test
+test2 = proj(test1)
+print(test2)
+
